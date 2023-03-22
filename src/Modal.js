2c86ed986d642931d37c6768be6ea81ed0cd7843
tazolomtadze <76435828+tazolomtadze@users.noreply.github.com>
2023-03-22 20:09:29 +0400

    Lil changes

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
 const Modal = () => {
-  return <h2>Modal</h2>
-}
-
-export default Modal
+  return (
+    <div className={"modal-overlay  "}>
+      <div className="modal-container">
+        <h4> Modal Content</h4>
+        <button className="close-modal-btn">
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Modal;
